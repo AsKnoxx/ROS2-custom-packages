845838bca1ac08a334bf3a77da7e9d5938b49f56
AsKnoxx <nodtas14@gmail.com>
2025-05-03 11:03:07 +0100

    Write constructor, destructor, a few basic methods

--- src/maglock_control/src/gpio_control.h
+++ src/maglock_control/src/gpio_control.h
@@ -2,12 +2,34 @@
 #define GPIO_CONTROL_H
 
 #include <JetsonGPIO.h>
+#include <map>
 
 class GPIOControl
 {
 	public:
+		// Constructs an object that uses board numbering method
+		GPIOControl(int mode = GPIO::BOARD);
+
+		// Destructor that cleans up all GPIO pins
+		~GPIOControl();
+
+		// Sets a pin to either input or output
+		void setupPin(int pin, int direction);
+
+		// Sets a pin to HIGH
+		void setHigh(int pin);
+
+		// Sets a pin to LOW
+		void setLow(int pin);
+
+		// Switches a pin from HIGH to LOW and vice versa
+		void toggle(int pin);
+
+		// Gets the pin's state
+		void getState(int pin) const;
 
 	private:
+		std::map<int, bool> pinStates_;
 };
 
 #endif
